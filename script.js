4cd562a821f9c6aa0d4ef2d690740baffbce2aa1
visualeyes-portfolio <sjslatinsky@gmail.com>
2026-01-28 11:34:34 -0800

    Change background music file to a new track

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Background music setup
     // const bgMusic = new Audio('https://assets.mixkit.co/active_storage/sfx/123/123-preview.mp3');
-    const bgMusic = new Audio('./audio/bg_music_haal_kaisa_hai-1.mp3');
+    const bgMusic = new Audio('./audio/Valentines Day Song - No Deets.mp3');
     bgMusic.loop = true;
     // const cardFlipSound = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-game-card-flip-2517.wav');
     const cardFlipSound = new Audio('https://cdn.freesound.org/previews/442/442903_9359753-lq.mp3');
